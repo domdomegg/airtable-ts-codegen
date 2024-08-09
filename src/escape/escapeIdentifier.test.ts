615100bd461dcd6825fac89a6a52cbbe7179d260
Francis Nimick <fnimick@gmail.com>
2024-08-09 16:13:42 -0400

    Normalize identifier names by removing diacritics

--- src/escape/escapeIdentifier.test.ts
+++ src/escape/escapeIdentifier.test.ts
@@ -10,12 +10,11 @@ it.each([
   ['an example', 'AnExample'],
   [' an example', 'AnExample'],
   ['an  example', 'AnExample'],
-  [
-    "[example] this is a table! yes, even with symbols: \"'!.Id",
-    'ExampleThisIsATableYesEvenWithSymbolsId',
-  ],
+  ['[example] this is a table! yes, even with symbols: "\'!.Id', 'ExampleThisIsATableYesEvenWithSymbolsId'],
   ['if', 'If'],
   ['Date Field #1', 'DateField1'],
+  ['Special Character Fiéld', 'SpecialCharacterField'],
+  ['"Iлｔèｒｎåｔïｏｎɑｌíƶａｔï߀ԉ"', 'Internationalizati0n'],
 ])('should escape string: %s -> %s', (s1, s2?) => {
   expect(escapeIdentifier(s1)).toBe(s2 ?? s1);
 });
@@ -23,7 +22,7 @@ it.each([
 it.each([
   ['empty string', ''],
   ['whitespace', ' '],
-  ['symbols', "\"'`\t"],
+  ['symbols', '"\'`\t'],
   ['only numbers', '1234'],
 ])('should throw if unsalvageable: %s', (_, s) => {
   expect(() => escapeIdentifier(s)).toThrow();
--- src/escape/escapeIdentifier.ts
+++ src/escape/escapeIdentifier.ts
@@ -1,34 +1,35 @@
 import { recase } from '@kristiandupont/recase';
+import { remove } from 'diacritics';
 
 const toPascalCase = recase(null, 'pascal');
 
 /** Used for identifiers. If the name has symbols or is illegal Typescript, strip out symbols and make it PascalCase. */
 // NB: A wider set of things than are accepted by this function are valid identifiers in TypeScript (see https://stackoverflow.com/a/9337047). However, this works well for our purposes.
 export const escapeIdentifier = (name: string): string => {
-  const trimmed = name.trim();
+  const preprocessed = remove(name).trim();
   let isLegalIdentifier = true;
 
-  if (!/^[$A-Z_a-z][\w$]*$/.test(trimmed)) {
+  if (!/^[$A-Z_a-z][\w$]*$/.test(preprocessed)) {
     isLegalIdentifier = false;
   }
   try {
     // eslint-disable-next-line @typescript-eslint/no-implied-eval, no-new
-    new Function(`const ${trimmed} = 1;`);
+    new Function(`const ${preprocessed} = 1;`);
   } catch {
     isLegalIdentifier = false;
   }
 
   if (isLegalIdentifier) {
-    return trimmed;
+    return preprocessed;
   }
 
   // Remove all characters up to the first valid identifier start character (A-Z, a-z, $), then
   // replace all invalid characters with underscores, and finally collapse multiple underscores into one.
-  const validIdentifierStartIndex = trimmed.search(/[$A-Za-z]/);
+  const validIdentifierStartIndex = preprocessed.search(/[$A-Za-z]/);
   if (validIdentifierStartIndex === -1) {
     throw new Error(`Invalid and unsalvageable identifier: ${name}`);
   }
-  const snaked = trimmed
+  const snaked = preprocessed
     .slice(validIdentifierStartIndex)
     .replace(/[^$A-Z_a-z\d]/g, '_')
     .replace(/_+/g, '_');
