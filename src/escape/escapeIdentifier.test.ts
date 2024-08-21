d0fd13e3dbd70578b4803c6528ac451514fe9012
Francis Nimick <fnimick@gmail.com>
2024-08-21 09:39:37 -0400

    Various improvements to generated output (#3)
    * Preserve non-starting numerals in airtable field names
    * Skip fields whose types we do not support
    Outputs skipped fields as comments in the generated file.
    * Normalize identifier names by removing diacritics

--- src/escape/escapeIdentifier.test.ts
+++ src/escape/escapeIdentifier.test.ts
@@ -10,19 +10,20 @@ it.each([
   ['an example', 'AnExample'],
   [' an example', 'AnExample'],
   ['an  example', 'AnExample'],
-  [
-    "[example] this is a table! yes, even with symbols: \"'!.Id",
-    'ExampleThisIsATableYesEvenWithSymbolsId',
-  ],
+  ['[example] this is a table! yes, even with symbols: "\'!.Id', 'ExampleThisIsATableYesEvenWithSymbolsId'],
   ['if', 'If'],
-])('should escape string: %s -> %s', (s1, s2) => {
+  ['Date Field #1', 'DateField1'],
+  ['Special Character Fiéld', 'SpecialCharacterField'],
+  ['"Iлｔèｒｎåｔïｏｎɑｌíƶａｔï߀ԉ"', 'Internationalizati0n'],
+])('should escape string: %s -> %s', (s1, s2?) => {
   expect(escapeIdentifier(s1)).toBe(s2 ?? s1);
 });
 
 it.each([
   ['empty string', ''],
   ['whitespace', ' '],
-  ['symbols', "\"'`\t"],
+  ['symbols', '"\'`\t'],
+  ['only numbers', '1234'],
 ])('should throw if unsalvageable: %s', (_, s) => {
   expect(() => escapeIdentifier(s)).toThrow();
 });
